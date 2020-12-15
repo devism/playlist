3b7faa9ab5d877c6e36c70d23ab5234545940b42
oxvac <oxvac@bitbucket.org>
2020-12-15 13:15:36 -0800

    can now display folder names

--- src/playlistLinks/PlaylistLinks.js
+++ src/playlistLinks/PlaylistLinks.js
@@ -3,8 +3,7 @@ import firebase from "firebase/app";
 import storage from "firebase/storage";
 import  firebaseConfig from "../dropzone/config";
 
-// get list of directories that were made from the input field
-// eventually will have to be per account 
+
 
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
@@ -17,28 +16,44 @@ let fb_storage = firebase.storage();
 let storageRef = fb_storage.ref();  
 
 let rootRef = storageRef.root;
-// console.log(rootRef.fullPath);
-// console.log(rootRef.listAll());
 
-rootRef.listAll().then(function(res) {
-    res.prefixes.forEach(function(folderRef) {
-       console.log(folderRef.name)
-      // All the prefixes under listRef.
-      // You may call listAll() recursively on them.
-    });
-    res.items.forEach(function(itemRef) {
-      // All the items under listRef.
-      console.log(itemRef);
-    });
-  }).catch(function(error) {
-    // Uh-oh, an error occurred!
-  });
+let folders = [];
 
+   
 
 const PlayListLinks = () =>{
+
+const [folders, setFolders] = useState([]);    
+
+useEffect(() => {
+    rootRef.listAll().then(function(res) {
+        let temp = [];
+        res.prefixes.forEach(function(folderRef) {
+            // console.log(folderRef.name)
+            temp.push(folderRef.name)
+            
+            // All the prefixes under listRef.
+            // You may call listAll() recursively on them.
+        });
+        setFolders(temp);
+        res.items.forEach(function(itemRef) {
+            // All the items under listRef.
+            // console.log(itemRef.name);
+        });
+        }).catch(function(error) {
+        // Uh-oh, an error occurred!
+        }); 
+},[])
+   
+console.log(folders);
+
     return(
         <>
-            <p>https://play.list/oxvac</p>
+            <ul>
+                {folders.map((folder, index) => {
+                    return <li key={index}> {folder}</li>
+                })}
+            </ul>
         </>
     )
 }
